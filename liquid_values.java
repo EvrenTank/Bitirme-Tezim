d5317d7b574c49451fdbb4e9da4261f9f6e01d3e
EvrenTank <75124278+EvrenTank@users.noreply.github.com>
2022-05-11 17:04:15 +0300

    Add files via upload

--- liquid_values.java
+++ liquid_values.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class liquid_values {
 
 
+
     // ısıl iletkenlik=> A,B,C,Tmin,Tmax  birimi: W/(mK)  Kontrol et
 
 
@@ -37,194 +38,174 @@ public class liquid_values {
 
 
 
+    ArrayList <String[]> cp_katsayılar= new ArrayList<String[]>(); // İlk olarak dosyalarda okunulan satırları kaydetmek için ArrayList oluşturuyorum.
+    ArrayList <String[]> critical_katsayılar= new ArrayList<String[]>();// İlk olarak dosyalarda okunulan satırları kaydetmek için ArrayList oluşturuyorum.
+    ArrayList <String[]> density_katsayılar= new ArrayList<String[]>(); // İlk olarak dosyalarda okunulan satırları kaydetmek için ArrayList oluşturuyorum.
+    ArrayList <String[]> hvap_katsayılar= new ArrayList<String[]>();// İlk olarak dosyalarda okunulan satırları kaydetmek için ArrayList oluşturuyorum.
+    ArrayList <String[]> viscosity_katsayılar= new ArrayList<String[]>();// İlk olarak dosyalarda okunulan satırları kaydetmek için ArrayList oluşturuyorum.
 
-    public String[] read_all_Files(){
- File myFile1= new File("D:\\Kullanıcılar-Lenovo-silme\\eclipse-workspace\\Bitirme Tezi\\src\\bitirme_tezi\\katsayılar\\cp.txt");
- File myFile2= new File("D:\\Kullanıcılar-Lenovo-silme\\eclipse-workspace\\Bitirme Tezi\\src\\bitirme_tezi\\katsayılar\\critical values.txt");
- File myFile3= new File("D:\\Kullanıcılar-Lenovo-silme\\eclipse-workspace\\Bitirme Tezi\\src\\bitirme_tezi\\katsayılar\\density.txt");
- File myFile4= new File("D:\\Kullanıcılar-Lenovo-silme\\eclipse-workspace\\Bitirme Tezi\\src\\bitirme_tezi\\katsayılar\\hvap.txt");
- File myFile5= new File("D:\\Kullanıcılar-Lenovo-silme\\eclipse-workspace\\Bitirme Tezi\\src\\bitirme_tezi\\katsayılar\\viscosity.txt");
- ArrayList <String> cp_katsayılar= new ArrayList<String>();
- ArrayList <String> critical_katsayılar= new ArrayList<String>();
- ArrayList <String> density_katsayılar= new ArrayList<String>();
- ArrayList <String> hvap_katsayılar= new ArrayList<String>();
- ArrayList <String> viscosity_katsayılar= new ArrayList<String>();
-        Scanner myReader = null;
-        try {
-            myReader = new Scanner(myFile1);
-            while(myReader.hasNextLine()){
-                String data = myReader.nextLine();
-                cp_katsayılar.add(data);
 
-            }
-        } catch (FileNotFoundException e) {
 
-            e.printStackTrace();
+
+
+
+    public ArrayList<String[]> read_file(File file){
+    String[] a=null;
+    ArrayList <String []> katsayılar = new ArrayList< String[] >();
+    Scanner myReader;
+    try {
+        myReader = new Scanner(file);
+        while(myReader.hasNextLine()){
+            String data = myReader.nextLine();
+            String veri[]= data.split(" ");
+            katsayılar.add(veri);
+
         }
-        try {
-            myReader = new Scanner(myFile2);
-            while(myReader.hasNextLine()){
-                String data = myReader.nextLine();
-                critical_katsayılar.add(data);
+    } catch (FileNotFoundException e) {
 
-            }
-        } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
 
-            e.printStackTrace();
-        }
-        try {
-            myReader = new Scanner(myFile3);
-            while(myReader.hasNextLine()){
-                String data = myReader.nextLine();
-                density_katsayılar.add(data);
 
-            }
-        } catch (FileNotFoundException e) {
 
-            e.printStackTrace();
-        }
-        try {
-            myReader = new Scanner(myFile4);
-            while(myReader.hasNextLine()){
-                String data = myReader.nextLine();
-                hvap_katsayılar.add(data);
+    return  katsayılar;
+}
+    public void  read_all_Files(){
+ File cpvalues_File= new File("D:\\Kullanıcılar-Lenovo-silme\\eclipse-workspace\\Bitirme Tezi\\src\\bitirme_tezi\\katsayılar\\cp.txt");
+ File criticalvalues_File= new File("D:\\Kullanıcılar-Lenovo-silme\\eclipse-workspace\\Bitirme Tezi\\src\\bitirme_tezi\\katsayılar\\critical values.txt");
+ File densityvalues_File= new File("D:\\Kullanıcılar-Lenovo-silme\\eclipse-workspace\\Bitirme Tezi\\src\\bitirme_tezi\\katsayılar\\density.txt");
+ File hvapvalues_File= new File("D:\\Kullanıcılar-Lenovo-silme\\eclipse-workspace\\Bitirme Tezi\\src\\bitirme_tezi\\katsayılar\\hvap.txt");
+ File viscosityvalues_File= new File("D:\\Kullanıcılar-Lenovo-silme\\eclipse-workspace\\Bitirme Tezi\\src\\bitirme_tezi\\katsayılar\\viscosity.txt");
 
-            }
-        } catch (FileNotFoundException e) {
+   cp_katsayılar= read_file(cpvalues_File);
+   critical_katsayılar =read_file(criticalvalues_File);
+   density_katsayılar= read_file(densityvalues_File);
+   hvap_katsayılar= read_file(hvapvalues_File);
+   viscosity_katsayılar= read_file(viscosityvalues_File);
 
-            e.printStackTrace();
-        }
-        try {
-            myReader = new Scanner(myFile5);
-            while(myReader.hasNextLine()){
-                String data = myReader.nextLine();
-                viscosity_katsayılar.add(data);
 
-            }
-        } catch (FileNotFoundException e) {
 
-            e.printStackTrace();
-        }
+
         String temp_array[];
-        String cp_values [] =new String[cp_katsayılar.size()] ;
-        String density_values [] =new String[density_katsayılar.size()] ;
-        String critical_values [] =new String[critical_katsayılar.size()] ;
-        String hvap_values [] =new String[hvap_katsayılar.size()] ;
-        String viscosity_values [] =new String[viscosity_katsayılar.size()] ;
-        for (int i=0;i<cp_katsayılar.size();i++){
-            cp_values[i] = cp_katsayılar.get(i);
+        String cp_values [] =new String[cp_katsayılar.size()] ; // ArrayListleri, arraylere dönüşürdüm.
+        String density_values [] =new String[density_katsayılar.size()] ;// ArrayListleri, arraylere dönüşürdüm.
+        String critical_values [] =new String[critical_katsayılar.size()] ;// ArrayListleri, arraylere dönüşürdüm.
+        String hvap_values [] =new String[hvap_katsayılar.size()] ;// ArrayListleri, arraylere dönüşürdüm.
+        String viscosity_values [] =new String[viscosity_katsayılar.size()] ;// ArrayListleri, arraylere dönüşürdüm.
+//        for (int i=0;i<cp_katsayılar.size();i++){
+//            cp_values[i] = String.valueOf(cp_katsayılar.get(i).split(" "));
+//
+//
+//        }
+//        for (int i=0;i<density_katsayılar.size();i++){
+//            density_values[i] = density_katsayılar.get(i);
+//            temp_array= density_values[i].split(" ");
+//            for(int k=0;k<temp_array.length;k++){
+//                System.out.println(temp_array[k]);
+//            }
+//           }
+//        for (int i=0;i<critical_katsayılar.size();i++){
+//            critical_values[i] = critical_katsayılar.get(i);}
+//        for (int i=0;i<hvap_katsayılar.size();i++){
+//            hvap_values[i] = hvap_katsayılar.get(i);}
+//        for (int i=0;i<viscosity_katsayılar.size();i++){
+//            viscosity_values[i] = viscosity_katsayılar.get(i);}
 
-
-        }
-        for (int i=0;i<density_katsayılar.size();i++){
-            density_values[i] = density_katsayılar.get(i);}
-        for (int i=0;i<critical_katsayılar.size();i++){
-            critical_values[i] = critical_katsayılar.get(i);}
-        for (int i=0;i<hvap_katsayılar.size();i++){
-            hvap_values[i] = hvap_katsayılar.get(i);}
-        for (int i=0;i<viscosity_katsayılar.size();i++){
-            viscosity_values[i] = viscosity_katsayılar.get(i);}
-
-        for(int i=0; i<1000;i++){
-            System.out.println(cp_values[i]);
-            System.out.println(density_values[i]);
-            System.out.println(critical_values[i]);
-            System.out.println(hvap_values[i]);
-            System.out.println(viscosity_values[i]);
-        }
-
-
-
-
-return cp_values;
+//        for(int i=0; i<1000;i++){
+//           System.out.println(cp_values[i]);
+//           System.out.println(density_values[i]);
+//           System.out.println(critical_values[i]);
+//           System.out.println(hvap_values[i]);
+//           System.out.println(viscosity_values[i]);
+//        }
 }
     public double[] getcp(String name){
-        Field[] fields=this.getClass().getDeclaredFields();
-        double ro[]=null;
-        String nameofliquid="ro_"+name;
-        for(int i=0;i<fields.length;i++) {
-            if(nameofliquid.equals(fields[i].getName())){
-                fields[i].setAccessible(true);
-                try {
-                    ro= (double[]) fields[i].get(this);
+        double cp []=null;
+        for(String [] i:cp_katsayılar){
+            if( i[0] == name){
+                cp = new double[i.length-1];
+                cp [0] = Double.parseDouble(i[1]);
+                cp [1] = Double.parseDouble(i[2]);
+                cp [2] = Double.parseDouble(i[3]);
+                cp [3] = Double.parseDouble(i[4]);
+                cp [4] = Double.parseDouble(i[5]);
+                cp [5] = Double.parseDouble(i[6]);
 
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
             }
         }
-        return ro;
+        return cp;
     }
     public double[] getro(String name){
-        Field[] fields=this.getClass().getDeclaredFields();
-        double ro[]=null;
-        String nameofliquid="ro_"+name;
-        for(int i=0;i<fields.length;i++) {
-            if(nameofliquid.equals(fields[i].getName())){
-                fields[i].setAccessible(true);
-                try {
-                    ro= (double[]) fields[i].get(this);
+        double ro []=null;
+        for(String [] i:density_katsayılar){
+            if( i[0] == name){
+                ro = new double[i.length-1];
+                ro [0] = Double.parseDouble(i[1]);
+                ro [1] = Double.parseDouble(i[2]);
+                ro [2] = Double.parseDouble(i[3]);
+                ro [3] = Double.parseDouble(i[4]);
+                ro [4] = Double.parseDouble(i[5]);
+                ro [5] = Double.parseDouble(i[6]);
+                ro [6] = Double.parseDouble(i[7]);
+                ro [7] = Double.parseDouble(i[8]);
 
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
             }
         }
         return ro;
     }
 
     public double[] getvis(String name){
-        Field[] fields=this.getClass().getDeclaredFields();
-        double vis[]=null;
-        String nameofliquid="vis_"+name;
-        for(int i=0;i<fields.length;i++) {
-            if(nameofliquid.equals(fields[i].getName())){
-                fields[i].setAccessible(true);
-                try {
-                    vis= (double[]) fields[i].get(this);
+        double vis []=null;
+        for(String [] i:viscosity_katsayılar){
+            if( i[0] == name){
+                vis = new double[i.length-1];
+                vis [0] = Double.parseDouble(i[1]);
+                vis [1] = Double.parseDouble(i[2]);
+                vis [2] = Double.parseDouble(i[3]);
+                vis [3] = Double.parseDouble(i[4]);
+                vis [4] = Double.parseDouble(i[5]);
+                vis [5] = Double.parseDouble(i[6]);
+                vis [6] = Double.parseDouble(i[7]);
 
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
             }
         }
-        return vis;
-    }
+        return vis;}
 
-    public double[] getk(String name){
-        Field[] fields=this.getClass().getDeclaredFields();
-        double k[]=null;
-        String nameofliquid="k_"+name;
-        for(int i=0;i<fields.length;i++) {
-            if(nameofliquid.equals(fields[i].getName())){
-                fields[i].setAccessible(true);
-                try {
-                    k= (double[]) fields[i].get(this);
+    public double[] gethvap(String name){
+        double hvap []=null;
+        for(String [] i:hvap_katsayılar){
+            if( i[0] == name){
+                hvap = new double[i.length-1];
+                hvap [0] = Double.parseDouble(i[1]);
+                hvap [1] = Double.parseDouble(i[2]);
+                hvap [2] = Double.parseDouble(i[3]);
+                hvap [3] = Double.parseDouble(i[4]);
+                hvap [4] = Double.parseDouble(i[5]);
+                hvap [5] = Double.parseDouble(i[6]);
+                hvap [6] = Double.parseDouble(i[7]);
 
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
             }
         }
-        return k;
-    }
+        return hvap;}
 
-    public double[] getref(String name){
-        Field[] fields=this.getClass().getDeclaredFields();
-        double ref[]=null;
-        String nameofliquid="ref_"+name;
-        for(int i=0;i<fields.length;i++) {
-            if(nameofliquid.equals(fields[i].getName())){
-                fields[i].setAccessible(true);
-                try {
-                    ref= (double[]) fields[i].get(this);
+    public double[] get_critical(String name){
 
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
+        double critic []=null;
+        for(String [] i:critical_katsayılar){
+            if( i[0] == name){
+                critic = new double[i.length-1];
+                critic [0] = Double.parseDouble(i[1]);
+                critic [1] = Double.parseDouble(i[2]);
+                critic [2] = Double.parseDouble(i[3]);
+                critic [3] = Double.parseDouble(i[4]);
+                critic [4] = Double.parseDouble(i[5]);
+                critic [5] = Double.parseDouble(i[6]);
+                critic [6] = Double.parseDouble(i[7]);
+                critic [7] = Double.parseDouble(i[8]);
+
             }
         }
-        return ref;
+        return critic;
     }
 
 
